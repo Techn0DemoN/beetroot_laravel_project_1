421080086658488cae9f6e5ac9e1124773107895
Pasha <redmi2.misan@gmail.com>
2020-12-17 21:21:24 +0200

    Add posts and users seeders.

---
+++ database/seeds/PostsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $posts[] = [
+                'user_id' => rand(1,2),
+                'title' => str_random(10),
+                'description' => str_random(50),
+                'content' => str_random(150),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ];
+        }
+        DB::table('posts')->insert($posts);
+    }
+}
