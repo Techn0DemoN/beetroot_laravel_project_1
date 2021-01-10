905ded836bf8eadb659f2259ef688073dea898ac
mandar1nka <vladparamey1@ukr.net>
2021-01-10 22:55:33 +0300

    Add like System/ category with posts

---
+++ app/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use Sluggable;
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Post');
+    }
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+}
